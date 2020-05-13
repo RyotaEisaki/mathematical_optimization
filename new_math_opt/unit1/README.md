f428f5eaa71fb5d5034c4b3f207ecc57341a6bbb
Ryota Eisaki <ryotaeisaki0530@icloud.com>
2020-05-13 15:10:30 +0900

    add example

--- new_math_opt/unit1/README.md
+++ new_math_opt/unit1/README.md
@@ -1,1 +1,7 @@
 #　第一章　数理最適化入門
+
+引数の与え方
+ x1=model.addVar(0,GRB.INFINITY, 15, GRB.CONTINUOUS, "x1")
+ 下限、上限、目的関数の係数、変数のタイプ、名前
+
+ 
---
+++ new_math_opt/unit1/example.py
@@ -0,0 +1,36 @@
+from gurobipy import *
+
+model = Model("lo1")
+
+# 変数の定義
+x1 = model.addVar(vtype="C", name="x1")
+x2 = model.addVar(name="x2")
+x3 = model.addVar(ub=30,name="x3")
+
+model.update()
+
+# 制約
+model.addConstr(2*x1+x2+x3<= 60)
+#別の書き方
+# L1 = LinExpr([2,1,1]),[x1,x2,x3]
+# model.addConstr(L1,"<",60)
+# 別の書き方
+# model.addConstr(lhs=L1,sense="<",rhs=60)
+
+model.addConstr(x1+2*x2+x3<= 60)
+
+# 目的関数
+# 目的関数の方向を省略する場合
+# model.ModelSense=-1 #最小化＋1が規定値
+
+model.setObjective(15*x1+18*x2+30*x3,GRB.MAXIMIZE)
+
+# 最適化
+model.optimize()
+
+# 目的関数値の出力
+print("Opt. Value=", model.ObjVal)
+
+# 最適解の出力
+for v in model.getVars():
+    print(v.VarName, v.X)
