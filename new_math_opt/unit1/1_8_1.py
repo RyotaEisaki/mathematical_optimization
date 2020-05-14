7ff680d0d93468d076325976f7c1ebe2067fcc41
Ryota Eisaki <ryotaeisaki0530@icloud.com>
2020-05-14 22:12:40 +0900

    1_8

---
+++ new_math_opt/unit1/1_8_1.py
@@ -0,0 +1,22 @@
+from gurobipy import *
+
+# 分数最適化
+# 整数に限らない
+
+model = Model("fractional 1")
+x=model.addVjar()
+y=model.addVar()
+z=model.addVar()
+t=model.addVar()
+
+model.update()
+
+model.addConstr(x+y+z==32*t)
+model.addConstr(2*x+4*y==1)
+model.addConstr(2*x+4*y+8*z==80*t)
+model.setObjective(x+y,GRB.MINIMIZE)
+
+model.optimize()
+
+print("Opt. Val.", model.ObjVal, ", t=", t.X)
+print("(x,y,z)=", x.X/t.X, y.X/t.X, z.X/t.X)
