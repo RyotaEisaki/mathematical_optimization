2c0ffff1a81d858fd92c32868e35e467a9b463b2
Ryota Eisaki <ryotaeisaki0530@icloud.com>
2020-06-01 13:48:36 +0900

    3_1

--- new_math_opt/unit3/3_1.py
+++ new_math_opt/unit3/3_1.py
@@ -15,11 +15,38 @@ def CuttingStockExample():
 # First Decreasing: FFD 
 # サイズの大きいアイテムを順番に開いている瓶に入れる方法
 def FFD(s,B):
-    remain=[B]
-    sol=[[]]
-    for item in sorted(s,recerse=True):
+    remain=[B] # 現在使える人の残りサイズ
+    sol=[[]] # 解を保管するリスト
+    for item in sorted(s,recerse=True): # アイテムのサイズを大きい順に取り出している
         for (j,free) in emumerate(remain):
             if free >= item:
                 remain[j]-=item
                 sol[j].append(item)
                 break
+            else:
+                sol.append([item])
+                remain.append(B-item)
+    return sol
+
+def bpp(s,B):
+    n=len(s)
+    U=len(FFD(s,B))
+    model=Model("bpp")
+    x,y={}, {}
+    for i in range(n):
+        for j in range(U):
+            x[i,j]=model.addVar(vtype="B")
+    for j in range(U):
+        y[j]=model.addVar(vtype="B")
+    model.update()
+    for i in range(n):
+        model.addConstr(quicksum(x[i,j] for j in range(U)) ==1)
+    for j in range(U):
+        model.addConstr(quicksums[i]*x[i,j] for i in range(n))<+B*y[i]))
+    for j in range(U):
+        for i in range(n):
+            model.addConstr(x[i,j]<=y[j])
+    model.setObjective(quicksum(y[j] for j in range(U)), GRB.MINIMIZE)
+    model.update()
+    model.__data=x,y
+    return model
