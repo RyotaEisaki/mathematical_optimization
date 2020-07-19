bee40b6d305b6cff986b2544ffcc202a01d2905d
Ryota Eisaki <ryotaeisaki0530@icloud.com>
2020-07-19 15:40:32 +0900

    edit

--- python_math_opt/code/revised_simplex.py
+++ python_math_opt/code/revised_simplex.py
@@ -5,7 +5,7 @@ MEPS = 1.0e-10
 def lp_RevisedSimplex(c,A,b):
     np.seterr(divide='ignore')
     (m,n)=A.shape
-    AI=np.hstach((A,np.identity(m)))
+    AI=np.hstack((A,np.identity(m)))
     c0=np.r_[c,np.zeros(m)]
     basis=[n+i for i in range(m)]
     nonbasis=[j for j in range(m)]
@@ -36,3 +36,8 @@ def lp_RevisedSimplex(c,A,b):
             #　基底十匹邸の入れ替え
             nonbasis[s],basis[r]=basis[r],nonbasis[s]
 
+A=np.array([[2,2,-1],[2,-2,3],[0,2,-1]])
+c=np.array([4,3,5])
+b=np.array([6,8,4])
+
+lp_RevisedSimplex(c,A,b)
